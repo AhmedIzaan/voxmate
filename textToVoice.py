bcb1a7512d595e2bcf76c6770f6f68dcf3b284d9
Ahmed Izaan <ahmedizaanofficial@gmail.com>
2025-06-23 07:18:00 +0500

    created module textToVoice

---
+++ textToVoice.py
@@ -0,0 +1,19 @@
+
+import pyttsx3
+
+# Initializing the TTS engine 
+engine = pyttsx3.init()
+
+
+
+engine.setProperty('rate', 180) # Adjusting the speed of the speech
+
+def speak(text):
+    """
+    Converts a text string to speech.
+    """
+    if text:
+        print(f"VoxMate says: {text}")
+        engine.say(text)
+        engine.runAndWait()
+
