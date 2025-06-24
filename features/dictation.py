11d1a99cc805f34202e72e36406d90e3342cc92b
Ahmed Izaan <ahmedizaanofficial@gmail.com>
2025-06-25 00:24:33 +0500

    creating dictation.py
    adding the functionality of taking notes

---
+++ features/dictation.py
@@ -0,0 +1,9 @@
+# features/dictation.py
+
+def start_dictation():
+    """Returns an action to prompt the user for a file path to start dictation."""
+    return {
+        'action': 'start_dictation_prompt',
+        'speak': 'Okay, starting dictation. Please choose a file to save the notes to.'
+    }
+
